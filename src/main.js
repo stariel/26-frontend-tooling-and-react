961646c12771bc2384aa6e30c65a249a5344a5e2
Ariel Altaras <ariel.altaras@gmail.com>
2018-07-30 20:22:20 -0700

    created form and working on switching animal

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { say } from 'cowsay';
+import { say, CAT, FISH, FROG, GHOST, RABBIT } from 'cowsay';
 import faker from 'faker';
 
 import './style/app.scss';
@@ -14,14 +14,29 @@ class App extends React.Component {
 
         this.state = {
             content: null,
+            cow: '',
         };
 
         this.cowTalk = this.cowTalk.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.cowTalk();
+    }
+
+    handleChange(event) {
+        this.setState({cow: event.target.value});
+      }
 
     cowTalk() {
         let content = faker.fake("{{lorem.sentence}}");
-        ReactDOM.render(say({ text: content }), document.getElementById('cow'));
+        ReactDOM.render(say({
+             text: content,
+             cow: this.state.cow.toUpperCase(), 
+            }), document.getElementById('cow'));
     }
 
     render() {
@@ -31,7 +46,19 @@ class App extends React.Component {
                     <h1>Generate Cowsay Lorem</h1>
                 </header>
                 <pre id="cow"></pre>
-                <button onClick={this.cowTalk}>click me</button>
+                <form id="animal">
+                <label> Choose your animal:
+                <select value={this.state.cow} onChange={this.handleChange}>
+                <option value="">cow</option>
+                <option value="cat">cat</option>
+                <option value="fish">fish</option>
+                <option value="frog">frog</option>
+                <option value="ghost">ghost</option>
+                <option value="rabbit">rabbit</option>
+                </select>
+                </label>
+                </form>
+                <button type="submit" form="animal" onClick={this.handleSubmit}>click me</button>
             </div>
         )
     }
